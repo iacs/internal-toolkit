a0c30ac26cb7352996522733bf8e782993ed4c34
Iago Mosquera <flackon@gmail.com>
2023-05-16 16:52:25 +0200

    Quick scratchpad updates

--- pkg/01-desktop/rofi/quickscratch.sh
+++ pkg/01-desktop/rofi/quickscratch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 copy=$(xclip -o)
-input=$(echo "" | rofi -dmenu -location 0 -theme-str 'listview {enabled:false;} window {width: 50%;}' -p "Enter note for scratchpad > " -filter $copy)
+input=$(echo "" | rofi -dmenu -location 0 -theme-str 'listview {enabled:false;} window {width: 50%;}' -p "Enter note for scratchpad > " -filter "$copy")
+
 
 if [ "$input" ]
 then
-  jrnl sc "$input" && notify-send -u low "Added note" "$input" -a "Scratchpad"
+  jrnl sc "$input" && notify-send -u low "Added note" "$input" -a "Scratchpad" || notify-send -u high "Could not save note"
 else
   exit 1
 fi
